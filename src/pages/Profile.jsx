54fac31ca8669ab8c61a1e9edac68db551768bde
Arif Ullah <arifkhattak736@gmail.com>
2026-05-16 20:31:19 +0500

    changes done

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import axios from "axios";
 import { serverUrl } from "../App";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { setProfileData } from "../redux/userSlice";
+import { setProfileData, setUserData } from "../redux/userSlice";
 import { useEffect } from "react";
 import { FaArrowLeft } from "react-icons/fa";
-
+import dp from "../assets/dp.jpg";
+import Navbar from "../components/Navbar";
 const Profile = () => {
   const { userName } = useParams();
   const dispatch = useDispatch();
   const { profileData } = useSelector((state) => state.user);
-
+  const navigate = useNavigate();
   const handleProfile = async () => {
     console.log("profile clicke...");
     try {
@@ -30,21 +31,88 @@ const Profile = () => {
     handleProfile();
   }, [userName]);
 
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  const handleLogOut = async () => {
+    try {
+      const result = await axios.get(`${serverUrl}/api/v1/auth/signout`, {
+        withCredentials: true,
+      });
+      dispatch(setUserData(null));
+      console.log(result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="w-full min-h-screen  bg-black">
       {/* top content */}
-      <div className="text-white flex items-center justify-between w-full p-5">
-        <FaArrowLeft />
-        <p>username</p>
-        <button>Log Out</button>
+      <div className="text-white flex items-center justify-between w-full h-[80px] px-[30px]">
+        <FaArrowLeft
+          className=" text-white w-[25px] h-[25px] "
+          onClick={handleBack}
+        />
+        <p className="text-semibold  text-[20px]">{userName}</p>
+        <button
+          onClick={handleLogOut}
+          className="font-semibold cursor-pointer text-[20px] text-blue-500"
+        >
+          Log Out
+        </button>
       </div>
 
       {/* user profile */}
-      <div className="w-lvh">
+      <div className="flex  items-top justify-center w-full mt-10 gap-10 ">
         {/* profile image */}
-        <div></div>
+        <div className="w-20 h-20 rounded-full">
+          <img
+            src={dp}
+            alt="profile image"
+            className="w-full h-full object-cover rounded-full"
+          />
+        </div>
         {/* profile details */}
-        <div></div>
+        <div className="text-white">
+          <p>{profileData.name || "John Doe"}</p>
+          <p>{profileData.userName || "john_doe"}</p>
+          <p>{profileData.bio || "No bio available"}</p>
+        </div>
+      </div>
+
+      {/* post followers folllowing detial */}
+      <div className="flex items-center justify-center gap-10 text-white mt-8">
+        {/* Post */}
+        <div className="">
+          <p>100</p>
+          <p>Posts </p>
+        </div>
+
+        {/* Followers */}
+        <div>
+          <p>100k</p>
+          <p>Followers </p>
+        </div>
+
+        {/* Following */}
+        <div>
+          <p>500</p>
+          <p>Following </p>
+        </div>
+      </div>
+
+      {/* Edit Profile Button */}
+      <div className="flex items-center justify-center mt-8">
+        <button className="bg-white  text-black font-bold py-2 px-12 rounded-full">
+          Edit Profile
+        </button>
+      </div>
+
+      {/* footer part */}
+      <div className="bg-white w-[60%] mx-auto h-48 rounded-t-2xl mt-5 flex items-center justify-center ">
+        <Navbar />
       </div>
     </div>
   );
