bfceae5c1fa5b3e97db11c9bbc44f18736b39403
Arif Ullah <arifkhattak736@gmail.com>
2026-06-11 23:08:21 +0500

    changes done

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import dp from "../assets/dp.jpg";
 import VideoPlayer from "./VideoPlayer";
-
+import { GoHeart } from "react-icons/go";
+import { GoHeartFill } from "react-icons/go";
+import { MdOutlineInsertComment } from "react-icons/md";
+import { useSelector } from "react-redux";
 const Post = ({ postData }) => {
+  const { userData } = useSelector((state) => state.user);
   return (
-    <div className="w-[90%]  flex flex-col gap-[10px] bg-white items-center  shadow-2xl shadow-[#00000058]  rounded-2xl pb-[20px]">
+    <div className="w-[90%]  flex flex-col gap-[10px] bg-white items-center   shadow-2xl shadow-[#00000058]  rounded-2xl pb-[20px]">
       <div className="w-full h-[80px] flex justify-between items-center px-[10px]">
         <div className="flex items-center justify-center gap-[20px]">
           <div className="w-[60px] h-[60px] rounded-full border-2 border-black cursor-pointer overflow-hidden">
@@ -39,6 +43,26 @@ const Post = ({ postData }) => {
             <VideoPlayer media={postData.media} />
           </div>
         )}
+      </div>
+
+      {/*  likes and comments section */}
+      <div className="w-full h-[60px] flex items-center justify-between px-[20px] mt-[10px] ">
+        {/* likes and comments */}
+        <div className="flex">
+          {/* likes */}
+          <div>
+            {!postData.likes.includes(userData._id) && <GoHeart />}
+            {postData.likes.includes(userData._id) && <GoHeartFill />}
+            <span className="">{postData.likes.length}</span>
+          </div>
+          {/* comments */}
+          <div>
+            <MdOutlineInsertComment />
+            <span className="">{postData.comments.length}</span>
+          </div>
+        </div>
+        {/* icon */}
+        <div></div>
       </div>
     </div>
   );
