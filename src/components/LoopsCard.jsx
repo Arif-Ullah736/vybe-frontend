cc6d92699e8f810503554b55d68539cf5c156b92
Arif Ullah <arifkhattak736@gmail.com>
2026-06-14 14:46:07 +0500

    changes done

--- src/components/LoopsCard.jsx
+++ src/components/LoopsCard.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useRef } from "react";
 
-const LoopsCard = () => {
-  return <div></div>;
+const LoopsCard = ({ loop }) => {
+  const videoRef = useRef();
+  return (
+    <div className="w-full lg:w-[480px] h-screen flex items-center justify-center border-l-2  border-r-2  border-gray-800 relative  ">
+      <video src={loop.media} ref={videoRef} loop></video>
+    </div>
+  );
 };
 
 export default LoopsCard;
--- src/pages/Loops.jsx
+++ src/pages/Loops.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import { FaArrowLeft } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import LoopsCard from "../components/LoopsCard";
+import { useSelector } from "react-redux";
 
 const Loops = () => {
   const navigate = useNavigate();
+  const { loopData } = useSelector((state) => state.loop);
   return (
     <div className="w-screen h-screen bg-black overflow-hidden  flex items-center justify-center  ">
-      <div className="fixed top-[10px] left-[10px]  w-full h-[80px] flex items-center gap-[20px] px-[20px]">
+      <div className="fixed top-[10px] left-[10px]  w-full h-[80px] flex items-center gap-[20px] px-[20px] z-100">
         <FaArrowLeft
           className="w-[25px] h-[25px] cursor-pointer  text-white"
           onClick={() => navigate(`/`)}
         />
         <h1 className="text-white text-[22px] font-semibold">Loops</h1>
+      </div>
+      <div className="h-[100vh] overflow-y-scroll  snap-y snap-mandatory scrollbar-none">
+        {loopData.map((loop, index) => (
+          <div className="h-screen snap-start">
+            <LoopsCard loop={loop} key={index} />
+          </div>
+        ))}
       </div>
     </div>
   );
