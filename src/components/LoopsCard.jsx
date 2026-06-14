453203a1efed75bc4ade7426c18de5ec46487eed
Arif Ullah <arifkhattak736@gmail.com>
2026-06-14 15:13:09 +0500

    changes done

--- src/components/LoopsCard.jsx
+++ src/components/LoopsCard.jsx
@@ -1,10 +1,38 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 
 const LoopsCard = ({ loop }) => {
   const videoRef = useRef();
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        const video = videoRef.current;
+        if (entry.isIntersecting) {
+          video.play();
+        } else {
+          video.pause();
+        }
+      },
+      { threshold: 0.6 },
+    );
+    if (videoRef.current) {
+      observer.observe(videoRef.current);
+    }
+    return () => {
+      if (videoRef.current) {
+        observer.unobserve(videoRef.current);
+      }
+    };
+  }, []);
   return (
-    <div className="w-full lg:w-[480px] h-screen flex items-center justify-center border-l-2  border-r-2  border-gray-800 relative  ">
-      <video src={loop.media} ref={videoRef} loop></video>
+    <div className="w-full lg:w-[480px] lg:mx-auto h-screen flex items-center justify-center bg-black relative overflow-hidden">
+      <video
+        src={loop.media}
+        ref={videoRef}
+        loop
+        // autoPlay
+        playsInline
+        className="w-full h-full object-cover"
+      />
     </div>
   );
 };
--- src/pages/Loops.jsx
+++ src/pages/Loops.jsx
@@ -8,18 +8,18 @@ const Loops = () => {
   const navigate = useNavigate();
   const { loopData } = useSelector((state) => state.loop);
   return (
-    <div className="w-screen h-screen bg-black overflow-hidden  flex items-center justify-center  ">
-      <div className="fixed top-[10px] left-[10px]  w-full h-[80px] flex items-center gap-[20px] px-[20px] z-100">
+    <div className="w-screen h-screen bg-black overflow-hidden relative">
+      <div className="fixed top-[10px] left-[10px] w-full h-[80px] flex items-center gap-[20px] px-[20px] z-50">
         <FaArrowLeft
-          className="w-[25px] h-[25px] cursor-pointer  text-white"
+          className="w-[25px] h-[25px] cursor-pointer text-white"
           onClick={() => navigate(`/`)}
         />
         <h1 className="text-white text-[22px] font-semibold">Loops</h1>
       </div>
-      <div className="h-[100vh] overflow-y-scroll  snap-y snap-mandatory scrollbar-none">
+      <div className="w-screen h-screen overflow-y-scroll snap-y snap-mandatory scrollbar-none scroll-smooth">
         {loopData.map((loop, index) => (
-          <div className="h-screen snap-start">
-            <LoopsCard loop={loop} key={index} />
+          <div key={index} className="h-screen snap-start flex-shrink-0">
+            <LoopsCard loop={loop} />
           </div>
         ))}
       </div>
