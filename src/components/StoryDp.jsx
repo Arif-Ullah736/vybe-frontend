382839d53296c71a164d74fd9d6f09ec280f6b7c
Arif Ullah <arifkhattak736@gmail.com>
2026-06-20 22:29:04 +0500

    changes done

--- src/components/StoryDp.jsx
+++ src/components/StoryDp.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import dp from "../assets/dp.jpg";
 import { LuCirclePlus } from "react-icons/lu";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
 import { serverUrl } from "../App";
+import { markStoryAsViewed } from "../redux/storySlice";
 
 const StoryDp = ({ userName, profileImage, story }) => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const { userData } = useSelector((state) => state.user);
+  const { viewedStories } = useSelector((state) => state.story);
+
+  const isCurrentUserStory = userName === "Your Story";
+  const isStoryViewed = story && viewedStories.includes(story._id);
 
   const handleViewers = async () => {
     try {
@@ -27,15 +33,31 @@ const StoryDp = ({ userName, profileImage, story }) => {
     } else if (story && userName === "Your Story") {
       handleViewers();
       navigate(`/story/${userData.userName}`);
-    } else {
+    } else if (story) {
       handleViewers();
+      // Mark other users' stories as viewed
+      dispatch(markStoryAsViewed(story._id));
       navigate(`/story/${userName}`);
     }
   };
+
+  // Change background based on viewed status
+  const getBackgroundClass = () => {
+    if (!story) return "";
+    if (isCurrentUserStory) {
+      return "bg-gradient-to-b from-blue-500 to-blue-950";
+    }
+    // For other users: blue if unviewed, gray if viewed
+    if (isStoryViewed) {
+      return "bg-gradient-to-b from-gray-400 to-gray-600";
+    }
+    return "bg-gradient-to-b from-blue-500 to-blue-950";
+  };
+
   return (
     <div className="w-[80px] flex flex-col">
       <div
-        className={` relative  flex items-center justify-center rounded-full w-[80px] h-[80px] ${story ? " bg-gradient-to-b from-blue-500 to-blue-950" : ""}`}
+        className={` relative  flex items-center justify-center rounded-full w-[80px] h-[80px] ${getBackgroundClass()}`}
       >
         <div
           className="w-[70px] h-[70px] rounded-full border-2 border-black cursor-pointer overflow-hidden rounded-full flex items-center justify-center"
--- src/redux/storySlice.js
+++ src/redux/storySlice.js
@@ -4,6 +4,7 @@ const initialState = {
   storyData: [],
   storyList: [],
   currentUserStory: null,
+  viewedStories: [],
 };
 
 const storySlice = createSlice({
@@ -19,10 +20,20 @@ const storySlice = createSlice({
     setCurrentUserStory: (state, action) => {
       state.currentUserStory = action.payload;
     },
+    markStoryAsViewed: (state, action) => {
+      const storyId = action.payload;
+      if (!state.viewedStories.includes(storyId)) {
+        state.viewedStories.push(storyId);
+      }
+    },
   },
 });
 
-export const { setStoryData, setStoryList, setCurrentUserStory } =
-  storySlice.actions;
+export const {
+  setStoryData,
+  setStoryList,
+  setCurrentUserStory,
+  markStoryAsViewed,
+} = storySlice.actions;
 
 export default storySlice.reducer;
