0ffc05ede83905e663c5c767e13077f9296a256d
Arif Ullah <arifkhattak736@gmail.com>
2026-06-22 01:06:23 +0500

    changes done

--- src/pages/MessageArea.jsx
+++ src/pages/MessageArea.jsx
@@ -9,15 +9,15 @@ const MessageArea = () => {
   const navigate = useNavigate();
   return (
     <div className="w-full h-screen bg-black relative ">
-      <div className="flex items-center gap-[15px] px-[20px] py-[10px] fixed top-0  z-100 bg-black w-full ">
-        <div className="  w-full h-[80px] flex items-center gap-[20px] px-[20px]">
+      <div className="flex items-center  gap-[15px] px-[20px] py-[10px] fixed top-0  z-100 bg-black w-full ">
+        <div className="   h-[80px] flex items-center gap-[20px] px-[20px]">
           <FaArrowLeft
             className="w-[25px] h-[25px] cursor-pointer  text-white "
             onClick={() => navigate(`/`)}
           />
         </div>
         <div
-          className="w-[30px] h-[30px] rounded-full border-2 border-black cursor-pointer overflow-hidden"
+          className="w-[40px] h-[40px] rounded-full border-2 border-black cursor-pointer overflow-hidden"
           onClick={() => navigate(`/profile/${selectedUser?.userName}`)}
         >
           <img
