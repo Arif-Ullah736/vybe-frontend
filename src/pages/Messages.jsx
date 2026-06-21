c558828242cc47f9f014902b3a1bf91edc09652d
Arif Ullah <arifkhattak736@gmail.com>
2026-06-21 23:53:12 +0500

    changes done

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { FaArrowLeft } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const Messages = () => {
+  const navigate = useNavigate();
   return (
-    <div>
+    <div className="w-full h-screen  flex flex-col bg-black gap-[20px] p-[20px] ">
       <div className="  w-full h-[80px] flex items-center gap-[20px] px-[20px]">
         <FaArrowLeft
           className="w-[25px] h-[25px] cursor-pointer  text-white"
