4535834698ffbddb99912203e285dd398dd5cd55
Arif Ullah <arifkhattak736@gmail.com>
2026-06-21 23:57:08 +0500

    changes done

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -8,10 +8,10 @@ const Messages = () => {
     <div className="w-full h-screen  flex flex-col bg-black gap-[20px] p-[20px] ">
       <div className="  w-full h-[80px] flex items-center gap-[20px] px-[20px]">
         <FaArrowLeft
-          className="w-[25px] h-[25px] cursor-pointer  text-white"
-          onClick={() => navigate(`/profile/${userData?.userName}`)}
+          className="w-[25px] h-[25px] cursor-pointer  text-white lg:hidden"
+          onClick={() => navigate(`/`)}
         />
-        <h1 className="text-white text-[22px] font-semibold">Edit Profile</h1>
+        <h1 className="text-white text-[22px] font-semibold">Messages</h1>
       </div>
     </div>
   );
