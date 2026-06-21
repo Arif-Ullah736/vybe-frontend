c67fbdf9c4d7d402ac9c0e5457ae3b7243cb8fd5
Arif Ullah <arifkhattak736@gmail.com>
2026-06-22 00:58:20 +0500

    changes done

--- src/pages/MessageArea.jsx
+++ src/pages/MessageArea.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { FaArrowLeft } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import dp from "../assets/dp.jpg";
 
 const MessageArea = () => {
   const { selectedUser } = useSelector((state) => state.message);
+  const navigate = useNavigate();
   return (
     <div className="w-full h-screen bg-black relative ">
-      <div className="flex items-center gap-[15px] px-[20px] py-[10px] fixed top-0  z-100 bg-black w-full "></div>
+      <div className="flex items-center gap-[15px] px-[20px] py-[10px] fixed top-0  z-100 bg-black w-full ">
+        <div className="  w-full h-[80px] flex items-center gap-[20px] px-[20px]">
+          <FaArrowLeft
+            className="w-[25px] h-[25px] cursor-pointer  text-white "
+            onClick={() => navigate(`/`)}
+          />
+        </div>
+        <div
+          className="w-[30px] h-[30px] rounded-full border-2 border-black cursor-pointer overflow-hidden"
+          onClick={() => navigate(`/profile/${selectedUser?.userName}`)}
+        >
+          <img
+            src={selectedUser?.profileImage || dp}
+            alt=""
+            className="w-full object-cover"
+          />
+        </div>
+      </div>
     </div>
   );
 };
