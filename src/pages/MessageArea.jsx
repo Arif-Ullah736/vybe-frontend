2eb4ee358a119fd98651d304630879fa6309b70e
Arif Ullah <arifkhattak736@gmail.com>
2026-06-22 00:50:22 +0500

    changes one

--- src/pages/MessageArea.jsx
+++ src/pages/MessageArea.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const MessageArea = () => {
-  return <div></div>;
+  const { selectedUser } = useSelector((state) => state.message);
+  return (
+    <div className="w-full h-screen bg-black relative ">
+      <div className="flex items-center gap-[15px] px-[20px] py-[10px] fixed top-0  z-100 bg-black w-full "></div>
+    </div>
+  );
 };
 
 export default MessageArea;
