33c13eee6964bd671c2f6b6ce76121c8439c8670
Arif Ullah <arifkhattak736@gmail.com>
2026-06-21 23:51:15 +0500

    changes done

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Messages = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="  w-full h-[80px] flex items-center gap-[20px] px-[20px]">
+        <FaArrowLeft
+          className="w-[25px] h-[25px] cursor-pointer  text-white"
+          onClick={() => navigate(`/profile/${userData?.userName}`)}
+        />
+        <h1 className="text-white text-[22px] font-semibold">Edit Profile</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Messages;
